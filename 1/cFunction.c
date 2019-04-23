386f72cd43ebde49e96d719b2a8ec916644ad8ea
neolnoel <48436334+neolnoel@users.noreply.github.com>
2019-04-23 14:58:56 +0300

    Create cFunction.c

---
+++ 1/cFunction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int parseSet(char *str, int argName[], int argValue[], int argCount) {
+    printf("%s\n", str);
+    return 0;
+}
+
+int parseFunc(char *str) {
+    printf("%s\n", str);
+    return 0;
+}
