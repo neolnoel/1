7c7983abba5331b02b747c8d54bdbe5137a13adc
neolnoel <48436334+neolnoel@users.noreply.github.com>
2019-04-23 15:15:00 +0300

    Update cFunction.c

--- 1/cFunction.c
+++ 1/cFunction.c
@@ -1,1 +1,38 @@
+/*
+Проектное задание №1.
+по консоли и ARGV / ARGC
+КЛЬКУЛЯТОР по примеру ввода ./calc ФУНКЦИЯ (y = 9 * x + 7) SET (x = 7)
+@author ИВАН
+@link http://moria.1586.su/moodle/mod/page/view.php?id=1705
+@link https://github.com/neolnoel
+*/
 
+#include <stdio.h>
+#include <string.h>
+#define ARG_AMOUNT 128
+
+/*
+Основная программа
+@uses <stdio.h>, <string.h>, errorChecker.c, calcFunctions.h
+*/
+int main(int argc, char *argv[]) {
+    int errorCode = 0, argCount = 0, argValue[ARG_AMOUNT], a, b;
+    char argName[ARG_AMOUNT], x, y;
+
+    if (argc < 2) {
+        errorCode = -1;
+        checkError(&errorCode);
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+            if (0 == strncmp(argv[i], "SET", strlen("SET"))) parseSet(argv[i], argName, argValue, argCount);
+            else if (0 == strncmp(argv[i], "FUNCTION", strlen("FUNCTION"))) parseFunc(argv[i], &a, &b, &x, &y);
+            else {
+                errorCode = -1;
+                checkError(&errorCode);
+            }
+    }
+    computeFunc(argName, argValue, argCount, &a, &b, &x, &y);
+    return 0;
+}
