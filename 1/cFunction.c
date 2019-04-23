8755b751e4ca0177b8ecdd1a6424de93a089498e
neolnoel <48436334+neolnoel@users.noreply.github.com>
2019-04-23 15:17:40 +0300

    Update and rename 1/cFunction.c to  1/cFunction.c

--- 1/cFunction.c
+++ 1/cFunction.c
@@ -1,38 +1,16 @@
 /*
-Проектное задание №1.
-по консоли и ARGV / ARGC
-КЛЬКУЛЯТОР по примеру ввода ./calc ФУНКЦИЯ (y = 9 * x + 7) SET (x = 7)
-@author ИВАН
-@link http://moria.1586.su/moodle/mod/page/view.php?id=1705
-@link https://github.com/neolnoel
+Проектное задание №1
+Код калькулятора корней функций
+@see main.c
 */
-
 #include <stdio.h>
-#include <string.h>
-#define ARG_AMOUNT 128
 
-/*
-Основная программа
-@uses <stdio.h>, <string.h>, errorChecker.c, calcFunctions.h
-*/
-int main(int argc, char *argv[]) {
-    int errorCode = 0, argCount = 0, argValue[ARG_AMOUNT], a, b;
-    char argName[ARG_AMOUNT], x, y;
+int parseSet(char *str, int argName[], int argValue[], int argCount) {
+    printf("%s\n", str);
+    return 0;
+}
 
-    if (argc < 2) {
-        errorCode = -1;
-        checkError(&errorCode);
-        return 0;
-    }
-
-    for (int i = 1; i < argc; i++) {
-            if (0 == strncmp(argv[i], "SET", strlen("SET"))) parseSet(argv[i], argName, argValue, argCount);
-            else if (0 == strncmp(argv[i], "FUNCTION", strlen("FUNCTION"))) parseFunc(argv[i], &a, &b, &x, &y);
-            else {
-                errorCode = -1;
-                checkError(&errorCode);
-            }
-    }
-    computeFunc(argName, argValue, argCount, &a, &b, &x, &y);
+int parseFunc(char *str, int *a, int *b, char *x, char *y) {
+    printf("%s\n", str);
     return 0;
 }
