942eed313177bdcb2f70ff4629ba863664f6a91b
neolnoel <48436334+neolnoel@users.noreply.github.com>
2019-04-25 20:12:16 +0300

    Update cFanction.h

--- 1/cFanction.h
+++ 1/cFanction.h
@@ -1,3 +1,6 @@
-
 int parseFunction(char *str, char *y, char *x, int *a, int *b);
 int parseSet(char *str, char *argName, int *argVal, int *argCnt);
+int computeFunction(char *argName, int *argVal, int argCnt, int a, int b, char x, char y);
+int getY(char *str, char *y);
+int getAX(char *str, char *x, int *a);
+int getB(char *str, int *b);
